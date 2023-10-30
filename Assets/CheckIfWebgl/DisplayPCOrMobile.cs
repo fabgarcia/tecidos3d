5e63a461285bb7f8260db1ef2fc67386a8343436
Fabiano Garcia <fabiano.garcia@mediasoft.com.br>
2023-10-30 15:15:28 -0300

    prefabs and checkifwebgl

---
+++ Assets/CheckIfWebgl/DisplayPCOrMobile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if !UNITY_EDITOR && UNITY_WEBGL
+using System.Runtime.InteropServices;
+#endif
+
+//namespace CheckIfMobileForUnityWebGL.Samples
+//{
+    
+    [RequireComponent(typeof(Text))]
+    public class DisplayPCOrMobile : MonoBehaviour
+    {
+        public bool isMobile;
+        
+        private void Start()
+        {
+            isMobile = false;
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+            isMobile = IsMobile();
+#endif
+
+            GetComponent<Text>().text = isMobile ? "Mobile" : "PC";
+        }
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+        [DllImport("__Internal")]
+        private static extern bool IsMobile();
+#endif
+ //   }
+}
